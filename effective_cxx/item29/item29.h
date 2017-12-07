195bec41de77a7a465999848c0f1529dc3937793
ckelsel <ckelsel@hotmail.com>
2017-12-07 17:38:47 +0800

    item29

---
+++ effective_cxx/item29/item29.h
@@ -0,0 +1,141 @@
+/* Copyright 2017 kunming.xie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __ITEM29_H__
+#define __ITEM29_H__
+#include <istream>
+#include <fstream>
+#include <memory> // std::tr1::shared_ptr
+
+#include <windows.h>
+#define Mutex CRITICAL_SECTION
+#define init InitializeCriticalSection
+#define lock EnterCriticalSection
+#define unlock LeaveCriticalSection
+
+class Image {
+public:
+    Image(std::istream &imgsrc, bool exception = true) {
+        if (exception) {
+            throw std::invalid_argument("invalid argument");
+        }
+    }
+};
+
+class PrettyMenuException {
+public:
+    PrettyMenuException() : changes_(0) {
+        init(&mutex_);
+        image_ = new Image(std::istream(NULL), false);
+    }
+    
+    void ChangeBackground(std::istream &imgsrc);
+
+    Image* GetImage() { return image_; }
+
+    int GetChange() { return changes_; }
+
+private:
+    Mutex mutex_;
+    Image *image_;
+    int changes_;
+};
+
+// caller should init mutex
+class Lock {
+public:
+    explicit Lock(Mutex *mutex) : mutex_(mutex) {
+        lock(mutex_);
+    }
+    ~Lock() {
+        unlock(mutex_);
+    }
+
+private:
+    Lock(const Lock &);
+    Lock& operator=(const Lock &);
+
+private:
+    Mutex *mutex_;
+};
+
+//////////////////
+
+class PrettyMenu_Exception_RefactorMutex {
+public:
+    PrettyMenu_Exception_RefactorMutex() : changes_(0) {
+        init(&mutex_);
+        image_ = new Image(std::istream(NULL), false);
+    }
+
+    Image* GetImage() { return image_; }
+
+    void ChangeBackground(std::istream &imgsrc);
+
+    int GetChange() { return changes_; }
+
+private:
+    Mutex mutex_;
+    Image *image_;
+    int changes_;
+};
+
+//////////////////
+
+class PrettyMenu_BasicExceptionSafe_RefactorMutex_SmartPointer {
+public:
+    PrettyMenu_BasicExceptionSafe_RefactorMutex_SmartPointer() : changes_(0) {
+        init(&mutex_);
+        image_.reset(new Image(std::istream(NULL), false));
+    }
+
+    Image* GetImage() { return image_.get(); }
+
+    void ChangeBackground(std::istream &imgsrc, bool exception = true);
+
+    int GetChange() { return changes_; }
+
+private:
+    Mutex mutex_;
+    std::tr1::shared_ptr<Image> image_;
+    int changes_;
+};
+
+//////////////////
+
+struct PrettyMenuImpl {
+    std::tr1::shared_ptr<Image> image_;
+    int changes_;
+};
+
+class PrettyMenu_StrongExceptionSafe_RefactorMutex_SmartPointer {
+public:
+    PrettyMenu_StrongExceptionSafe_RefactorMutex_SmartPointer() {
+        init(&mutex_);
+        impl_.reset(new PrettyMenuImpl());
+        impl_->image_.reset(new Image(std::istream(NULL), false));
+    }
+
+    Image* GetImage() { return impl_->image_.get(); }
+
+    int GetChange() { return impl_->changes_; }
+
+    void ChangeBackground(std::istream &imgsrc, bool exception = true);
+
+private:
+    Mutex mutex_;
+    std::tr1::shared_ptr<PrettyMenuImpl> impl_;
+};
+
+#endif // __ITEM29_H__
---
+++ testing/effective_cxx/item29_unittest.cc
@@ -0,0 +1,95 @@
+/* Copyright 2017 kunming.xie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "gtest/gtest.h"
+#include "item29/item29.h"
+
+TEST(EFFECTIVE_CXX, ITEM29) {
+    std::istream nullimg(NULL);
+    PrettyMenuException menu;
+    EXPECT_THROW({
+        try {
+            menu.ChangeBackground(nullimg);
+        } catch (const std::invalid_argument &e) {
+            ASSERT_STREQ("invalid argument", e.what());
+
+            // NULL for delete old image
+            ASSERT_EQ(menu.GetImage(), (Image *)NULL);
+
+            ASSERT_EQ(menu.GetChange(), 0);
+            throw;
+        }
+    }, std::invalid_argument);
+}
+
+TEST(EFFECTIVE_CXX, ITEM29_2) {
+    std::istream nullimg(NULL);
+    PrettyMenu_Exception_RefactorMutex menu;
+    EXPECT_THROW({
+        try {
+            menu.ChangeBackground(nullimg);
+        } catch (const std::invalid_argument &e) {
+            ASSERT_STREQ("invalid argument", e.what());
+
+            // NULL for delete old image
+            ASSERT_EQ(menu.GetImage(), (Image *)NULL);
+            ASSERT_EQ(menu.GetChange(), 0);
+            throw;
+        }
+    }, std::invalid_argument);
+}
+
+TEST(EFFECTIVE_CXX, ITEM29_3) {
+    std::istream nullimg(NULL);
+    PrettyMenu_BasicExceptionSafe_RefactorMutex_SmartPointer menu;
+    EXPECT_THROW({
+        try {
+            menu.ChangeBackground(nullimg);
+        } catch (const std::invalid_argument &e) {
+            ASSERT_STREQ("invalid argument", e.what());
+
+            ASSERT_NE(menu.GetImage(), (Image *)NULL);
+
+            // new Image throw exception, ++change not run
+            ASSERT_EQ(menu.GetChange(), 0);
+            throw;
+        }
+    }, std::invalid_argument);
+
+    menu.ChangeBackground(nullimg, false);
+    ASSERT_NE(menu.GetImage(), (Image *)NULL);
+    ASSERT_EQ(menu.GetChange(), 1);
+}
+
+TEST(EFFECTIVE_CXX, ITEM29_4) {
+    std::istream nullimg(NULL);
+    PrettyMenu_StrongExceptionSafe_RefactorMutex_SmartPointer menu;
+    EXPECT_THROW({
+        try {
+            menu.ChangeBackground(nullimg);
+        } catch (const std::invalid_argument &e) {
+            ASSERT_STREQ("invalid argument", e.what());
+
+            ASSERT_NE(menu.GetImage(), (Image *)NULL);
+
+            // new Image throw exception, ++change not run
+            ASSERT_EQ(menu.GetChange(), 0);
+            throw;
+        }
+    }, std::invalid_argument);
+
+    menu.ChangeBackground(nullimg, false);
+    ASSERT_NE(menu.GetImage(), (Image *)NULL);
+    ASSERT_EQ(menu.GetChange(), 1);
+}
